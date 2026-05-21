2e02b4a334878f4e8d29631e2433a18ecc1e3d32
Dany Duboille <danyduboille@yahoo.fr>
2026-05-21 18:41:26 +0200

    feat: Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return children;
+  return (
+    <>
+      {children}
+      <SpeedInsights />
+    </>
+  );
 }
